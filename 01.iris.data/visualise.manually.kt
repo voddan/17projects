3487bdc220137f141aa4c9351aafa43910ac032f
voddan <dgvodopyan@gmail.com>
2017-11-27 08:08:45 +0300

    Started TornadoFX staff for plots

--- 01.iris.data/visualise.manually.kt
+++ 01.iris.data/visualise.manually.kt
@@ -2,8 +2,9 @@ package iris.data.visualise.manually
 
 import java.io.File
 import javafx.application.Application
+import javafx.collections.*
 import javafx.geometry.Orientation
-import javafx.scene.chart.NumberAxis
+import javafx.scene.chart.*
 import tornadofx.*
 import java.time.LocalDate
 import java.time.temporal.ChronoUnit
@@ -42,4 +43,53 @@ fun main(args: Array<String>) {
     val measurments = lines.filterNot { it.isEmpty() }.map { Measurment.parse(it) }
 
     measurments.forEach { println(it) }
+
+    Application.launch(MyApp::class.java, *args)
+
+}
+
+class MyApp : App(MyView::class)
+
+class MyView(measurments: List<Measurment>) : View() {
+    override val root = vbox {
+        tableview<Measurment> {
+            column("a", Measurment::a)
+            column("b", Measurment::b)
+            column("c", Measurment::c)
+            column("d", Measurment::d)
+            column("TYPE", Measurment::type)
+
+            items.setAll(measurments)
+        }
+
+        areachart("area chart", NumberAxis(), NumberAxis()) {
+            for(m in measurments) {
+
+                series("") {
+
+                }
+
+
+            }
+        }
+    }
+}
+
+fun <X, Y, ChartType : XYChart<X, Y>> ChartType.data(x: X, y: Y,
+                                                     seriesName: String,
+                                                     op: ((XYChart.Data<X, Y>).() -> Unit)? = null): XYChart.Data<X, Y> {
+    val = data.find { it.name == seriesName }
+
+    val series = XYChart.Series<X, Y>()
+    series.name = name
+    if (elements != null) series.data = elements
+    if (op != null) op(series)
+    data.add(series)
+    return series
+}
+
+fun <X, Y> XYChart.Series<X, Y>.data(x: X, y: Y, extra: Any? = null, op: ((XYChart.Data<X, Y>).() -> Unit)? = null) = XYChart.Data<X, Y>(x, y).apply {
+    if (extra != null) extraValue = extra
+    data.add(this)
+    if (op != null) op(this)
 }
